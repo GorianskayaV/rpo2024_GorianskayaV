22e2e13560a6ae0f91acdf33256684a54b688fb9
vlada <gvea21u541@bmstu.student.ru>
2024-04-13 12:50:50 +0300

    Лаба 6

--- backend/src/main/java/ru/iu3/backend/models/Country.java
+++ backend/src/main/java/ru/iu3/backend/models/Country.java
@@ -1,15 +1,27 @@
 package ru.iu3.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "countries")
 @Access(AccessType.FIELD)
+
 public class Country {
     public Country() { }
     public Country(Long id) {
         this.id = id;
     }
+
+//    @OneToMany(mappedBy = "country")
+//    public List artists = new ArrayList();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "country")
+    public List<Artist> artists = new ArrayList<Artist>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
